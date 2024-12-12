9d666ea54bbdf9dc56fbd10d6aed57cecc1425dc
Mikhail Kondrashov <55343404+ckarandash@users.noreply.github.com>
2024-12-12 10:18:16 +0300

    initial commit

---
+++ src/circle.py
@@ -0,0 +1,32 @@
+import math
+
+def area(r):
+    """Возвращает площадь круга"""
+    # Параметры:
+    #   r (int): радиус круга
+    # Возвращаемое значение:
+    #   math.pi * r * r: искомая площадь круга
+
+    if type(r) is not int:
+        raise TypeError("Argument должен быть int")
+
+    if r <= 0:
+        raise ValueError("Argument должны быть больше нуля")
+
+    return math.pi * r * r
+
+def perimeter(r):
+    """Возвращает периметр круга"""
+    # Параметры:
+    #   r (int): радиус круга
+    # Возвращаемое значение:
+    #   2 * math.pi * r: искомый периметр круга
+
+    if type(r) is not int:
+        raise TypeError("Argument должен быть int")
+
+    if r <= 0:
+        raise ValueError("Argument должны быть больше нуля")
+
+    return 2 * math.pi * r
+
